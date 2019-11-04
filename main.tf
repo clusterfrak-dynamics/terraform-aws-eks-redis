b60796710466c326142820d7075e23214a6d7505
Kevin Lefevre <lefevre.kevin@gmail.com>
2019-11-04 14:56:33 +0100

    fix: description
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_elasticache_replication_group" "redis" {
   number_cache_clusters         = var.redis_cluster
   port                          = var.redis_port
   replication_group_id          = format("%.20s", "${var.name}-${var.env}")
-  replication_group_description = "Terraform-managed ElastiCache replication group for ${var.name}-${var.env}-${var.vpc_id}"
+  replication_group_description = "Terraform-managed ElastiCache replication group for ${var.name}-${var.env}"
   subnet_group_name             = aws_elasticache_subnet_group.redis_subnet_group.name
   security_group_ids            = [var.redis_security_group]
   transit_encryption_enabled    = var.redis_transit_encryption_enabled
