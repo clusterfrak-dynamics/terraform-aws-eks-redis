715da405db0c0b995acdd15dd26a444cf169ff67
Kevin Lefevre <lefevre.kevin@gmail.com>
2019-11-29 15:46:25 +0100

    feat: update provider
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- providers.tf
+++ providers.tf
@@ -17,7 +17,7 @@ provider "kubernetes" {
 provider "helm" {
   install_tiller                  = true
   service_account                 = "tiller"
-  tiller_image                    = "gcr.io/kubernetes-helm/tiller:v2.15.2"
+  tiller_image                    = "gcr.io/kubernetes-helm/tiller:v2.16.1"
   automount_service_account_token = true
 
   kubernetes {
